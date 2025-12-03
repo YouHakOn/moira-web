a1c142bd66fab0e0685dad95b3066e9ec8e67b63
suyeon <jso8787@naver.com>
2025-12-04 03:10:03 +0900

    feat: establish WebSocket using SockJS with error handling #8 - Create WebSocket via SockJS and pass to webSocketFactory - Add error handling for WebSocket connection - Add global polyfill in Vite config

--- src/features/connect-socket/hooks/useConnectSocket.ts
+++ src/features/connect-socket/hooks/useConnectSocket.ts
@@ -1,18 +1,27 @@
+import SockJS from 'sockjs-client'
 import { Client } from '@stomp/stompjs'
 import { useEffect } from 'react'
 
-export function useConnectSocket(projectId: string) {
+export function useConnectSocket(projectId: number | undefined) {
   useEffect(() => {
     if (!projectId) return
+    console.log(projectId)
     // 소켓 클라이언트 생성
     const client = new Client({
-      brokerURL: 'ws://localhost:8080/ws',
+      webSocketFactory: () => new SockJS('http://localhost:8080/ws'),
       connectHeaders: {
         Authorization: `Bearer ${localStorage.getItem('accessToken')}`
       },
       reconnectDelay: 5000, // 재연결 옵션
+      debug: (msg) => console.log('[STOMP]', msg), // 디버그 메시지
+      /*
+        [STOMP] Opening Web Socket...
+        useConnectSocket.ts:16 [STOMP] Connection closed to http://localhost:8080/ws
+        useConnectSocket.ts:16 [STOMP] STOMP: scheduling reconnection in 5000ms
+        TODO: Spring Security → WebSocket endpoint 허용 여부 확인
+      */
       onConnect: () => {
-        console.log(console.log(`프로젝트 ${projectId} 소켓 연결됨!`))
+        console.log(`프로젝트 ${projectId} 소켓 연결됨!`)
         // crdt topic 구독
         client.subscribe(`/topic/crdt/${projectId}`, (message) =>
           console.log(`Received: ${message.body}`)
@@ -20,9 +29,13 @@ export function useConnectSocket(projectId: string) {
       },
       onStompError: (frame) => {
         console.error('STOMP 에러:', frame)
-      }
+      },
+      onWebSocketError: (evt) => console.error('WebSocket ERROR:', evt),
+      onDisconnect: () => console.log('Disconnected')
     })
+
     client.activate()
+
     return () => {
       client.deactivate()
       console.log(`프로젝트 ${projectId} 소켓 연결 끊김`)
